9e619c40558d37bce8566fc1ba2f7c4f837d9c52
joseaugustolima <66323877+JoseAugustoLima@users.noreply.github.com>
2022-09-21 14:23:13 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,14 +29,14 @@ resource "aws_instance" "demo_server" {
   tags = {
     Name = "Demo-TFC"
   }
-  
+ 
  resource "aws_s3_bucket" "b" {
   bucket = "my-tf-test-bucket"
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
   }
-}
+ }
 
 resource "aws_s3_bucket_acl" "example" {
   bucket = aws_s3_bucket.b.id
